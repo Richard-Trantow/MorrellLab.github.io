a1794f650b46a5958043861b3124459e2a0ba7bd
mojaveazure <hoff0792@umn.edu>
2016-07-21 08:21:54 -0500

    A script to update citation counts from Google Scholar

--- UpdateCitations.py
+++ UpdateCitations.py
@@ -4,24 +4,80 @@ import sys
 if not sys.version_info.major == 3 and not sys.version_info >= 3:
     sys.exit("Please use Python 3.3 or higher for this script")
 
-import re, os, argparse
+import os
 try:
-    import bibtexparser
     import yaml
-    from scholar.scholar import *
+    import scholar.scholar as s
 except ImportError as error:
     sys.exit("FAIL: " + error.name)
 
-#   Opena  document and turn it into a dictionary
-l = yaml.load_all(open('_publications/2016-Environmental-Association.md', 'r')).__next__()
+#   A function to use scholar.py to send a query and get information about citations
+def send_query(cluster):
+    """Send a query to Google Scholar and get number of citations and citation url"""
+    try:
+        assert isinstance(cluster, int)
+    except AssertionError:
+        raise
+    querier = s.ScholarQuerier()
+    settings = s.ScholarSettings()
+    settings.set_citation_format(settings.CITFORM_NONE)
+    querier.apply_settings(settings)
+    query = s.ClusterScholarQuery(cluster)
+    querier.send_query(query)
+    ncites = querier.articles[0].attrs['num_citations'][0]
+    curl = querier.articles[0].attrs['url_citations'][0]
+    return(ncites, curl)
 
-#   Search for and get stuff
-querier = ScholarQuerier()
-query = ClusterScholarQuery(8363220939686393422)
-settings = ScholarSettings()
-settings.set_citation_format(settings.CITFORM_NONE)
-querier.apply_settings(settings)
-querier.send_query(query)
-querier.articles[0].attrs['num_citations'][0]
-querier.articles[0].attrs['url_citations'][0]
 
+#   A function to parse our yaml/markdown documents using yaml.load_all
+def parse_yaml(document):
+    """Read a yaml document and return a dictionary with the name of the yaml document and dictionary of contents"""
+    try:
+        assert isinstance(document, str)
+        this_yaml = (document, yaml.load_all(open('_publications/' + document, 'r')).__next__())
+        return this_yaml
+    except AssertionError:
+        raise
+    except FileNotFoundError as error:
+        sys.exit("Failed to find " + error.filename)
+
+
+#   A function to write our yaml/markdown documents
+def write_yaml(name, yaml_dict):
+    """Write a yaml document to file"""
+    try:
+        assert isinstance(name, str)
+        assert isinstance(yaml_dict, dict)
+    except AssertionError:
+        raise
+    with open('_publications/' + name, 'w') as y:
+        y.write('---')
+        for key, value in yaml_dict.items():
+            y.write(str(key) + ': ' + str(value))
+            y.write('\n')
+        y.write('---')
+
+
+def main():
+    #   Make sure we're in the right directory, exit if not
+    if '_publications' not in os.listdir() or 'MorrellLab.github.io' not in os.getcwd():
+        sys.exit("Can't find '_publications' directory. Please make sure you're in 'MorrellLab.github.io'")
+    #   Get a list of publications
+    print("Finding all publications", file=sys.stderr)
+    publication_list = os.listdir('_publications')
+    #   Read that sh!t in
+    print("Reading in the publications", file=sys.stderr)
+    yaml_list = map(parse_yaml, publication_list)
+    #   For every document in our list of yaml dictionaries
+    for doc in yaml_list:
+        #   given the yaml name and yaml dictionary
+        (name, yaml_dict) = doc # TUPLE UNPACKING :O
+        if 'cluster' in yaml_dict.keys(): # If we have a cluster ID
+            print("Updating citations in", name, sep=" ", file=sys.stderr)
+            ncites, curl = send_query(int(yaml_dict['cluster'])) # Get the number of citations and url
+            yaml_dict['num_citations'] = ncites # Add ncites to our yaml dictionary
+            yaml_dict['citation_url'] = curl # Add curl to our yaml dictionary
+            write_yaml(name, yaml_dict) # Write it out
+
+
+main()
--- _publications/2016-The-Effects.md
+++ _publications/2016-The-Effects.md
@@ -6,4 +6,5 @@ paper_title: "The effects of both recent and long-term selection and genetic dri
 paper_journal: "G3 6: 609-622"
 paper_url: "http://www.g3journal.org/content/early/2015/12/28/g3.115.024349.abstract"
 preprint: "http://biorxiv.org/content/early/2015/09/11/026625"
+cluster: 8363220939686393422
 ---
